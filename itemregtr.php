cffa2f7f1e0402c1feb226c315d73966ecf8a487
Beniedik V. Carreon <beniedik051502@gmail.com>
2022-06-01 16:40:34 +0800

    Enabled query for item type

--- itemregtr.php
+++ itemregtr.php
@@ -14,30 +14,19 @@ include 'dbconn.php';
                                 <label>Item</label>
                                 <select>
                                     <option value="">Select Your Option</option>
-                                    <option value="12 String Guitar">12 String Guitar</option>
-                                    <option value="Acoustic Guitar">Acoustic Guitar</option>
-                                    <option value="Bag">Bag</option>
-                                    <option value="Banduria">Banduria</option>
-                                    <option value="Cajon">Cajon</option>
-                                    <option value="Camera">Camera</option>
-                                    <option value="Camera Gimbal">Camera Gimbal</option>                                
-                                    <option value="Cello">Cello</option>                                
-                                    <option value="Classical Guitar">Classical Guitar</option>                                 
-                                    <option value="Classical Violin">Classical Violin</option>                           
-                                    <option value="DVD-Writer">DVD-Writer</option>                                    
-                                    <option value="Globe Prepaid WiFi">Globe Prepaid WiFi</option>                                    
-                                    <option value="Graphic Tablet">Graphic Tablet</option>                                  
-                                    <option value="Guitar">Guitar</option>                                    
-                                    <option value="Ipod">Ipod</option>                                    
-                                    <option value="Keyboard">Keyboard</option>   
-                                    <option value="Laptop">Laptop</option>                                    
-                                    <option value="Lens">Lens</option>                                    
-                                    <option value="Mouse">Mouse</option>                                   
-                                    <option value="Projector">Projector</option>                                    
-                                    <option value="Tablet">Tablet</option>   
-                                    <option value="Tripod">Tripod</option> 
-                                    <option value="Ukulele">Ukulele</option> 
-                                    <option value="Violin">Violin</option>                              
+<?php
+$getItemTypesQuery= "select itemtypeid, itemtypedesc from itemtype";
+$getItemTypesStmt= $dbh->query($getItemTypesQuery) or die(print_r($dbh->errorInfo(), true));
+
+foreach($getItemTypesStmt as $getItemTypesRow)
+{
+    $itemTypeId= $getItemTypesRow['itemtypeid'];
+    $itemTypeDesc= $getItemTypesRow['itemtypedesc'];
+?>
+                                    <option value="<?php echo $itemTypeId; ?>"><?php echo $itemTypeDesc;?></option>
+<?php
+}
+?>                            
                                 </select>
                             </div>
                             
