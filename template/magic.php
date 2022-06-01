ee0d43afb3e5a8f87748dcafd19bdaa95b4b668f
Beniedik V. Carreon <beniedik051502@gmail.com>
2022-06-01 16:44:46 +0800

    Session Management

---
+++ template/magic.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+if(isset($_SESSION['userId']) && isset($_SESSION['realName']) && isset($_SESSION['studnumber']))
+{
+    $loggedInUserId= $_SESSION['userId'];
+    $loggedInUserRealname= $_SESSION['realName'];
+    $loggedInAccessLevel= $_SESSION['studnumber'];
+    $isLoggedIn= $_SESSION['isLoggedIn'];
+}
+else
+{
+    session_unset();  
+    session_destroy();
+
+    $host  = $_SERVER['HTTP_HOST'];
+    $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+    header("Location:http://$host$uri/");
+}
