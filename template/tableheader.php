a6217fee6da5ff5650b406f35f4a203fd332d1dd
Beniedik V. Carreon <beniedik051502@gmail.com>
2022-06-01 19:26:59 +0800

    Adjust table columns

--- template/tableheader.php
+++ template/tableheader.php
@@ -1,9 +1,9 @@
 <table class="scroll hover" style="table-layout:fixed">
                         <thead>
                             <tr>
-                                <th style="width: 30%">Student Name</th>
+                                <th style="width: 40%">Student Name</th>
                                 <th style="width: 30%">Student Number</th>
-                                <th style="width: 30%">Item Type</th>
+                                <th style="width: 20%">Item Type</th>
                                 <th style="width: 10%"></th>								
                             </tr>
                         </thead>
