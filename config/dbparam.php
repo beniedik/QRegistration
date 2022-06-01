973d57bd9695cee72762c9d68ebcebb5aa094b16
Beniedik V. Carreon <beniedik051502@gmail.com>
2022-06-01 16:22:18 +0800

    Updated server IP

--- config/dbparam.php
+++ config/dbparam.php
@@ -2,7 +2,7 @@
 //dev server
 $deployType="Dev";
 #DAP VM IP: 192.168.227.130
-$hName = "192.168.266.128";
+$hName = "192.168.226.128";
 $dbPort = "5432";
 //DAP VM dbname: edgekit_dap
 $dbName = "wonderrams";
