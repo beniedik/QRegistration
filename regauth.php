8d1f947a90d4c60ae71ca49e483622ecb1d996e6
Beniedik V. Carreon <beniedik051502@gmail.com>
2022-06-01 19:32:07 +0800

    Minor change on query

--- regauth.php
+++ regauth.php
@@ -7,7 +7,7 @@ include_once 'dbconn.php';
 <div>
 <?php
 include_once 'template/tableheader.php';
-$getUnauthRegItemQuery= "select s.userid, s.studentname, s.studentidnumber, i.itemtypedesc from itemtype i, studentusers s, useritems u where u.userid=s.userid and u.itemtypeid=i.itemtypeid and u.is_approved <> true";
+$getUnauthRegItemQuery= "select u.useritemid, s.studentname, s.studentidnumber, i.itemtypedesc from itemtype i, studentusers s, useritems u where u.userid=s.userid and u.itemtypeid=i.itemtypeid and u.is_approved <> true order by u.useritemid asc";
 $getUnauthRegItemStmt= $dbh->query($getUnauthRegItemQuery) or die(print_r($dbh->errorInfo(), true));
 
 foreach($getUnauthRegItemStmt as $getUnauthRegItemRow)
@@ -21,7 +21,7 @@ foreach($getUnauthRegItemStmt as $getUnauthRegItemRow)
     <td><?php echo $userRealName; ?></td>
     <td><?php echo $userStudentNumber; ?></td>
     <td><?php echo $userItemType; ?></td>
-    <td><button>View</button></td>
+    <td><a href="#"><button>View</button></a></td>
 </tr>
 <?php
 }
