e72592294777d298cace5f1b7b0b92a09e3655ac
Beniedik V. Carreon <beniedik051502@gmail.com>
2022-06-01 19:26:02 +0800

    For presenting the content and review of item registered for approval

--- regauth.php
+++ regauth.php
@@ -5,9 +5,30 @@ include_once 'dbconn.php';
 ?>
 <h2>Registration List For Review</h2>
 <div>
-    <input type="text" placeholder="Student Name">
-    <input type="text" placeholder="Item Type">
-    <button>View</button>
+<?php
+include_once 'template/tableheader.php';
+$getUnauthRegItemQuery= "select s.userid, s.studentname, s.studentidnumber, i.itemtypedesc from itemtype i, studentusers s, useritems u where u.userid=s.userid and u.itemtypeid=i.itemtypeid and u.is_approved <> true";
+$getUnauthRegItemStmt= $dbh->query($getUnauthRegItemQuery) or die(print_r($dbh->errorInfo(), true));
+
+foreach($getUnauthRegItemStmt as $getUnauthRegItemRow)
+{
+    $userIdent= $getUnauthRegItemRow['userid'];
+    $userRealName= $getUnauthRegItemRow['studentname'];
+    $userStudentNumber = $getUnauthRegItemRow['studentidnumber'];
+    $userItemType= $getUnauthRegItemRow['itemtypedesc'];
+?>
+<tr>
+    <td><?php echo $userRealName; ?></td>
+    <td><?php echo $userStudentNumber; ?></td>
+    <td><?php echo $userItemType; ?></td>
+    <td><button>View</button></td>
+</tr>
+<?php
+}
+include_once 'template/tablefooter.php';
+?>
+
+    
 </div>
 <?php
 include_once 'template/footer.php';
--- template/tableheader.php
+++ template/tableheader.php
@@ -1,9 +1,10 @@
 <table class="scroll hover" style="table-layout:fixed">
                         <thead>
                             <tr>
-                                <th style="width: 10%">Student Name</th>
-                                <th style="width: 10%">Student Number</th>
-                                <th style="width: 10%">Item Type</th>								
+                                <th style="width: 30%">Student Name</th>
+                                <th style="width: 30%">Student Number</th>
+                                <th style="width: 30%">Item Type</th>
+                                <th style="width: 10%"></th>								
                             </tr>
                         </thead>
                         <tbody>
