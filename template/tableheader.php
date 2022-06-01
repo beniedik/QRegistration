e289631db402556b9774d100e57e2c483740dfb9
Beniedik V. Carreon <beniedik051502@gmail.com>
2022-06-01 19:16:47 +0800

    Table template

---
+++ template/tableheader.php
@@ -0,0 +1,9 @@
+<table class="scroll hover" style="table-layout:fixed">
+                        <thead>
+                            <tr>
+                                <th style="width: 10%">Student Name</th>
+                                <th style="width: 10%">Student Number</th>
+                                <th style="width: 10%">Item Type</th>								
+                            </tr>
+                        </thead>
+                        <tbody>
