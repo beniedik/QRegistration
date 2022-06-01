fa59afc52eb083c4b5df875afad1052f644a4d53
Beniedik V. Carreon <beniedik051502@gmail.com>
2022-06-01 18:08:09 +0800

    Updated UI

--- regauth.php
+++ regauth.php
@@ -1,5 +1,12 @@
 <?php
 include_once 'template/header.php';
 include_once 'template/magic.php';
-echo "Registration Authorization";
+?>
+<h2>Registration List For Review</h2>
+<div>
+    <input type="text" placeholder="Student Name">
+    <input type="text" placeholder="Item Type">
+    <button>View</button>
+</div>
+<?php
 include_once 'template/footer.php';
