9086ded1e43c5f10019e8dc9e92e85cf8a15523c
watermillow321 <89201568+watermillow321@users.noreply.github.com>
2022-06-02 14:32:59 +0800

    Minor fix of URL

--- index.php
+++ index.php
@@ -7,11 +7,11 @@ require_once("template/header.php");
   <h2>Log In</h2>
   <form>
     <div>
-      <a href="userlogin.php"><button>Student Login</button></a>
+      <a href="/userlogin.php"><button>Student Login</button></a>
       <span></span>
     </div>
     <div>
-      <a href="staffuserlogin.php"><button>Staff Login</button></a>
+      <a href="/staffuserlogin.php"><button>Staff Login</button></a>
       <span></span>
     </div>
   </form>
