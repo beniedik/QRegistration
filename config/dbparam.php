b6549004edd2c3b1eb2d18a01907a131e74b377f
watermillow321 <89201568+watermillow321@users.noreply.github.com>
2022-06-02 14:37:42 +0800

    Changed IP for Willow laptop

--- config/dbparam.php
+++ config/dbparam.php
@@ -1,8 +1,8 @@
 <?php
 //dev server
-$deployType="Dev";
+$deployType = "Dev";
 #DAP VM IP: 192.168.227.130
-$hName = "192.168.226.128";
+$hName = "192.168.107";
 $dbPort = "5432";
 //DAP VM dbname: edgekit_dap
 $dbName = "wonderrams";
