b5664e05cb8a927a13a785568e16094ee9a9c5bd
watermillow321 <89201568+watermillow321@users.noreply.github.com>
2022-06-02 21:00:17 +0800

    Logic for allowing or denying the user item

---
+++ allowuseritem.php
@@ -0,0 +1,22 @@
+<?php
+include_once 'dbconn.php';
+
+$useritemIdNumber = $_GET['id'];
+
+$setUserItemsToAllowed = "update useritems set is_reviewed=true, is_approved=true, approvaldate=NOW() where useritemid=$useritemIdNumber";
+try {
+    $dbh->beginTransaction();
+    $dbh->query($setUserItemsToFalse);
+    $dbh->commit();
+} catch (PDOException $e) {
+    $dbh->rollback();
+    echo "Failed to complete transaction: " . $e->getMessage() . "\n";
+    exit;
+}
+
+$host  = $_SERVER['HTTP_HOST'];
+$uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+$extra = 'regauth.php';
+
+//redirect to secured home page (home.php)
+header("Location:http://$host$uri/$extra");
