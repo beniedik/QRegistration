fe56d9b7157f76193566adef3a1bb80b23518740
watermillow321 <89201568+watermillow321@users.noreply.github.com>
2022-06-02 16:54:25 +0800

    Initial UI implementation

--- verifier.php
+++ verifier.php
@@ -1,10 +1,40 @@
 <?php
 include_once 'template/header.php';
-include_once 'template/magic.php';
 include_once 'dbconn.php';
 
 // Sanitize incoming username and password
-$userItemId= $_GET['id'];
+$studentIdNumber = $_GET['id'];
 
-echo "Verifier Page: user item ID is $userItemId";
-include_once 'template/footer.php';
+echo "Verifier Page: student ID number is $studentIdNumber";
+?>
+<h2>Information Screen</h2>
+<div>
+    <div>
+        <img>
+    </div>
+    <div>
+        <span>
+
+        </span>
+    </div>
+    <div><button>Scan Another</button></div><br>
+    <input type="text" placeholder="Student Name"><br>
+    <input type="text" placeholder="Student ID">
+    <div>
+        <div>
+            <img>
+        </div>
+        <div>
+            <span>
+
+            </span>
+        </div>
+        <input type="text" placeholder="Item Type">
+        <input type="text" placeholder="Item Information">
+
+        <input type="checkbox">
+    </div><br>
+    <div>
+    </div>
+    <?php
+    include_once 'template/footer.php';
