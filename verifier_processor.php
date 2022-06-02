25c15dfa5c96c4b31c354d164420baa5ca920ded
watermillow321 <89201568+watermillow321@users.noreply.github.com>
2022-06-02 19:00:56 +0800

    Corredted minor tag isue

--- verifier_processor.php
+++ verifier_processor.php
@@ -1,1 +1,14 @@
-<php include_once 'dbconn.php' ; $userItem=$_POST['userItem'] ; if ($_POST["Submit" ]=="Submit" ) { for ($i=0; $i<sizeof($userItem); $i++) { //$query="INSERT INTO employee (name) VALUES ('" .$userItem[$i]. "')" ; //mysql_query($query) or die(mysql_error()); echo "$userItem[$i]<br/>" ; } echo "Record is inserted" ; } ?>
+<?php
+include_once 'dbconn.php';
+
+$userItem= $_POST['userItem'];
+if ($_POST["Submit" ]=="Submit" )
+{
+    for ($i=0; $i<sizeof($userItem); $i++)
+    {
+        //$query="INSERT INTO employee (name) VALUES ('" .$userItem[$i]. "')";
+        //mysql_query($query) or die(mysql_error());
+        echo "$userItem[$i]<br/>";
+    }
+    echo "Record is inserted";
+}
