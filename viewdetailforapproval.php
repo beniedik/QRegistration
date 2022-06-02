2f5ab9f27fc6a9b9ef8219c8fd97b5d5043bb73e
watermillow321 <89201568+watermillow321@users.noreply.github.com>
2022-06-02 15:57:30 +0800

    Initial UI display

--- viewdetailforapproval.php
+++ viewdetailforapproval.php
@@ -6,5 +6,46 @@ include_once 'dbconn.php';
 // Sanitize incoming username and password
 $userItemId = $_GET['id'];
 //echo "user item ID is $userItemId";
-$getUerItemDetailQuery = "select u.useritemid, s.studentname, i.itemtypedesc, u.brand, u.model, u.serialnumber, u.color from studentusers as s, itemtype as i, useritems as u where u.userid=s.userid and u.itemtypeid=i.itemtypeid and u.useritemid=$userItemId";
-echo $getUerItemDetailQuery;
+$getUerItemDetailQuery = "select u.useritemid, s.studentname, s.studentidnumber, i.itemtypedesc, u.brand, u.model, u.serialnumber, u.color from studentusers as s, itemtype as i, useritems as u where u.userid=s.userid and u.itemtypeid=i.itemtypeid and u.useritemid=$userItemId";
+$getUerItemDetailStmt = $dbh->query($getUerItemDetailQuery) or die(print_r($dbh->errorInfo(), true));
+
+foreach ($getUerItemDetailStmt as $getUerItemDetailRow) {
+    $userIdent = $getUerItemDetailRow['useritemid'];
+    $userRealName = $getUerItemDetailRow['studentname'];
+    $userStudentNumber = $getUerItemDetailRow['studentidnumber'];
+    $itemDesc = $getUerItemDetailRow['itemtypedesc'];
+    $itemBrand = $getUerItemDetailRow['brand'];
+    $itemModel = $getUerItemDetailRow['model'];
+    $itemSN = $getUerItemDetailRow['serialnumber'];
+    $itemColor = $getUerItemDetailRow['color'];
+?>
+    <h2>Registration Details</h2>
+    <div>
+        <div>
+            <img>
+        </div>
+        <div>
+            <span>
+
+            </span>
+        </div>
+        <input type="text" placeholder="Student Name"><br>
+        <input type="text" placeholder="Student ID">
+        <div>
+            <div>
+                <img>
+            </div>
+            <div>
+                <span>
+
+                </span>
+            </div>
+            <input type="text" placeholder="Item Type">
+            <button>Approve</button><br>
+            <input type="text" placeholder="Item Information">
+            <button>Deny</button>
+        </div><br>
+        <div><button>Back</button></div>
+    </div>
+<?php
+}
