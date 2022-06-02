7842bc4fd0e1bedb28e41f179bfffa8c03cf46ff
watermillow321 <89201568+watermillow321@users.noreply.github.com>
2022-06-02 22:09:34 +0800

    Report generator code

---
+++ template/reporttableheader.php
@@ -0,0 +1,13 @@
+<table class="scroll hover" style="table-layout:fixed">
+    <thead>
+        <tr>
+            <th style="width: 40%">Student Name</th>
+            <th style="width: 30%">Student Number</th>
+            <th style="width: 20%">Item Type</th>
+            <th style="width: 10%">Brand</th>
+            <th style="width: 40%">Model</th>
+            <th style="width: 30%">Color</th>
+            <th style="width: 20%">Serial Number</th>
+        </tr>
+    </thead>
+    <tbody>
--- useritemreport.php
+++ useritemreport.php
@@ -1,4 +1,29 @@
 <?php
 include 'dbconn.php';
 
-echo "This is the Uer Item Report";
+$getUserItemsInCampusQuery = "select s.studentname, s.studentidnumber, i.itemtypedesc as itemtypedesc, u.brand as brand, u.model as model, u.serialnumber as serialnumber, u.color as color from studentusers as s, itemtype as i, useritems as u where u.userid=s.userid and u.itemtypeid=i.itemtypeid and u.is_in=true";
+$getUserItemsInCampusStmt = $dbh->query($getUserItemsInCampusQuery) or die(print_r($dbh->errorInfo(), true));
+include 'template/reporttableheader.php';
+
+foreach ($getUserItemsInCampusStmt as $getUserItemsInCampusRow) {
+    $studentName = $getUserItemsInCampusRow['studentname'];
+    $studentIdNumber = $getUserItemsInCampusRow['studentidnumber'];
+    $itemTypeDesc = $getUserItemsInCampusRow['itemtypedesc'];
+    $brand = $getUserItemsInCampusRow['brand'];
+    $model = $getUserItemsInCampusRow['model'];
+    $color = $getUserItemsInCampusRow['color'];
+    $itemSN = $getUserItemsInCampusRow['serialnumber'];
+?>
+    <tr>
+        <td><?php echo $studentName; ?></td>
+        <td><?php echo $studentIdNumber; ?></td>
+        <td><?php echo $itemTypeDesc; ?></td>
+        <td><?php echo $brand; ?></td>
+        <td><?php echo $model; ?></td>
+        <td><?php echo $color; ?></td>
+        <td><?php echo $itemSN; ?></td>
+    </tr>
+<?php
+}
+
+include 'template/tablefooter.php';
