79bc5c2a0e14bed6e209f8d3e76dbf616182a273
watermillow321 <89201568+watermillow321@users.noreply.github.com>
2022-06-02 15:00:01 +0800

    Streamlined the staff login logic

--- staffuserlogin_processor.php
+++ staffuserlogin_processor.php
@@ -3,40 +3,30 @@ include 'template/magic.php';
 include 'dbconn.php';
 
 // Sanitize incoming username and password
-$username= $_POST['usernameField'];
-$password= $_POST['passwordField'];
+$username = $_POST['usernameField'];
+$password = $_POST['passwordField'];
 
-$loginUsernameQuery="select userid, staffname, staffidnumber, staffroleid from staffusers where username='$username' and userpassword='$password'";
-$loginStmt= $dbh->query($loginUsernameQuery) or die(print_r($dbh->errorInfo(), true));
+$loginUsernameQuery = "select userid, staffname, staffidnumber, from staffusers where username='$username' and userpassword='$password'";
+$loginStmt = $dbh->query($loginUsernameQuery) or die(print_r($dbh->errorInfo(), true));
 
-foreach($loginStmt as $loginRow)
-{
-    $staffIdent= $loginRow['userid'];
-    $staffRealName= $loginRow['staffname'];
+foreach ($loginStmt as $loginRow) {
+    $staffIdent = $loginRow['userid'];
+    $staffRealName = $loginRow['staffname'];
     $userStaffNumber = $loginRow['staffidnumber'];
-    $staffRoleId= $loginRow['staffroleid'];
-
 }
 //echo "$staffIdent $staffRealName $userStaffNumber $staffRoleId";
 
-$isLoggedIn=TRUE;
+$isLoggedIn = TRUE;
 
 session_start();
-$_SESSION['userId']= $staffIdent;
-$_SESSION['realName']= $staffRealName;
-$_SESSION['studnumber']= $userStaffNumber;
-$_SESSION['isLoggedIn']=$isLoggedIn;
+$_SESSION['userId'] = $staffIdent;
+$_SESSION['realName'] = $staffRealName;
+$_SESSION['studnumber'] = $userStaffNumber;
+$_SESSION['isLoggedIn'] = $isLoggedIn;
 
 $host  = $_SERVER['HTTP_HOST'];
 $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
-if($staffRoleId == 1)
-{
-    $extra = 'regauth.php';
-}
-elseif($staffRoleId == 2)
-{
-    $extra = 'verifier.php';
-}
+$extra = 'regauth.php';
 
 //redirect to secured home page (home.php)
 header("Location:http://$host$uri/$extra");
