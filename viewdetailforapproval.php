2a19cf6339e615df7c05fb50974fe36bd33829a8
watermillow321 <89201568+watermillow321@users.noreply.github.com>
2022-06-02 15:39:59 +0800

    Query formulation

--- viewdetailforapproval.php
+++ viewdetailforapproval.php
@@ -4,5 +4,7 @@ include_once 'template/magic.php';
 include_once 'dbconn.php';
 
 // Sanitize incoming username and password
-$userItemId= $_GET['id'];
-echo "user item ID is $userItemId";
+$userItemId = $_GET['id'];
+//echo "user item ID is $userItemId";
+$getUerItemDetailQuery = "select u.useritemid, s.studentname, i.itemtypedesc, u.brand, u.model, u.serialnumber, u.color from studentusers as s, itemtype as i, useritems as u where u.userid=s.userid and u.itemtypeid=i.itemtypeid and u.useritemid=$userItemId";
+echo $getUerItemDetailQuery;
