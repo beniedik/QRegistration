50df0ba075726a3ec87f05c8fce2352e9799c358
watermillow321 <89201568+watermillow321@users.noreply.github.com>
2022-06-02 19:54:15 +0800

    Save and ask to close

--- verifier_processor.php
+++ verifier_processor.php
@@ -5,11 +5,22 @@ $userItem = $_POST['userItem'];
 if ($_POST["Submit"] == "Submit") {
     for ($i = 0; $i < sizeof($userItem); $i++) {
         $updateUserItemStatus = "update useritems set is_in=true where useritemid=$userItem[$i];";
-        echo $updateUserItemStatus;
-        echo "<br/>";
+        //echo $updateUserItemStatus;
+        //echo "<br/>";
+        try {
+            $dbh->beginTransaction();
+            $dbh->query($updateUserItemStatus);
+            $dbh->commit();
+        } catch (PDOException $e) {
+            $dbh->rollback();
+            echo "Failed to complete transaction: " . $e->getMessage() . "\n";
+            exit;
+        }
         //$query="INSERT INTO employee (name) VALUES ('" .$userItem[$i]. "')";
         //mysql_query($query) or die(mysql_error());
-        echo "$userItem[$i]<br/>";
+        //echo "$userItem[$i]<br/>";
     }
-    echo "Record is inserted";
+    //echo "Record is inserted";
 }
+
+echo "Changes saved, close this browser tab";
