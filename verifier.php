398789feda1c53311c219be97e8ccfae87bb9547
watermillow321 <89201568+watermillow321@users.noreply.github.com>
2022-06-03 20:57:25 +0800

    Code cleanup

--- verifier.php
+++ verifier.php
@@ -10,84 +10,75 @@ include_once 'dbconn.php';
                 <form id="itemreg-form" class="form" action="verifier_processor.php" method="post">
                     <h3 class="text-center text-info">Item Verification</h3>
                     <form action="verifier_processor.php" method="post">
-                        <div>
-                            <div>
-                                <img>
-                            </div>
-                            <div>
-                                <span>
+                        <?php
+                        // Sanitize incoming username and password
+                        $studentIdNumber = $_GET['id'];
 
-                                </span>
-                            </div>
-                            <?php
-                            // Sanitize incoming username and password
-                            $studentIdNumber = $_GET['id'];
+                        $getStudentDetailsQuery = "select distinct s.userid, s.studentname, s.studentidnumber from studentusers as s where s.studentidnumber='$studentIdNumber'";
+                        $getStudentDetailsStmt = $dbh->query($getStudentDetailsQuery) or die(print_r($dbh->errorInfo(), true));
 
-                            $getStudentDetailsQuery = "select distinct s.userid, s.studentname, s.studentidnumber from studentusers as s where s.studentidnumber='$studentIdNumber'";
-                            $getStudentDetailsStmt = $dbh->query($getStudentDetailsQuery) or die(print_r($dbh->errorInfo(), true));
-
-                            foreach ($getStudentDetailsStmt as $getStudentDetailsRow) {
-                                //
-                                $studentUserId = $getStudentDetailsRow['userid'];
-                                $studentName = $getStudentDetailsRow['studentname'];
-                                $studentIdNumber = $getStudentDetailsRow['studentidnumber'];
-                            ?>
-                                <input type="hidden" name="userid" value="<?php echo $studentUserId; ?>">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <h5 class="card-title"><?php echo $studentName; ?></h5>
-                                        <h6 class="card-subtitle mb-2 text-muted"><?php echo $studentIdNumber; ?></h6>
-                                    </div>
+                        foreach ($getStudentDetailsStmt as $getStudentDetailsRow) {
+                            //
+                            $studentUserId = $getStudentDetailsRow['userid'];
+                            $studentName = $getStudentDetailsRow['studentname'];
+                            $studentIdNumber = $getStudentDetailsRow['studentidnumber'];
+                        ?>
+                            <input type="hidden" name="userid" value="<?php echo $studentUserId; ?>">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title"><?php echo $studentName; ?></h5>
+                                    <h6 class="card-subtitle mb-2 text-muted"><?php echo $studentIdNumber; ?></h6>
                                 </div>
+                            </div>
 
-                            <?php
-                            }
-                            ?>
-                            <div class="form-group">
-                                <table class="table table-hover">
-                                    <thead>
+                        <?php
+                        }
+                        ?>
+                        <div class="form-group">
+                            <table class="table table-hover">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">Item Type</th>
+                                        <th scope="col">Brand</th>
+                                        <th scope="col">Model</th>
+                                        <th scope="col">Color</th>
+                                        <th scope="col">Serial #</th>
+                                        <th scope="col"></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
+                                    $getStudentItemsQuery = "select u.useritemid, i.itemtypedesc, u.brand, u.model, u.serialnumber, u.color, u.is_in from studentusers as s, itemtype as i, useritems as u where u.userid=s.userid and u.itemtypeid=i.itemtypeid and u.is_approved=true and s.studentidnumber='$studentIdNumber' order by useritemid asc";
+                                    $getStudentItemsStmt = $dbh->query($getStudentItemsQuery) or die(print_r($dbh->errorInfo(), true));
+
+                                    foreach ($getStudentItemsStmt as $getStudentItemsRow) {
+                                        //
+                                        $userItemId = $getStudentItemsRow['useritemid'];
+                                        $itemTypeDesc = $getStudentItemsRow['itemtypedesc'];
+                                        $itemBrand = $getStudentItemsRow['brand'];
+                                        $itemModel = $getStudentItemsRow['model'];
+                                        $itemSN = $getStudentItemsRow['serialnumber'];
+                                        $itemColor = $getStudentItemsRow['color'];
+                                        $isInStatus = $getStudentItemsRow['is_in'];
+                                    ?>
+
                                         <tr>
-                                            <th scope="col">Item Type</th>
-                                            <th scope="col">Brand</th>
-                                            <th scope="col">Model</th>
-                                            <th scope="col">Color</th>
-                                            <th scope="col">Serial #</th>
-                                            <th scope="col"></th>
+                                            <td><?php echo $itemTypeDesc; ?></td>
+                                            <td><?php echo $itemBrand; ?></td>
+                                            <td><?php echo $itemModel; ?></td>
+                                            <td><?php echo $itemColor; ?></td>
+                                            <td><?php echo $itemSN; ?></td>
+                                            <td><input name="userItem[]" value="12" type="checkbox" checked></td>
                                         </tr>
-                                    </thead>
-                                    <tbody>
-                                        <?php
-                                        $getStudentItemsQuery = "select u.useritemid, i.itemtypedesc, u.brand, u.model, u.serialnumber, u.color, u.is_in from studentusers as s, itemtype as i, useritems as u where u.userid=s.userid and u.itemtypeid=i.itemtypeid and u.is_approved=true and s.studentidnumber='$studentIdNumber' order by useritemid asc";
-                                        $getStudentItemsStmt = $dbh->query($getStudentItemsQuery) or die(print_r($dbh->errorInfo(), true));
-
-                                        foreach ($getStudentItemsStmt as $getStudentItemsRow) {
-                                            //
-                                            $userItemId = $getStudentItemsRow['useritemid'];
-                                            $itemTypeDesc = $getStudentItemsRow['itemtypedesc'];
-                                            $itemBrand = $getStudentItemsRow['brand'];
-                                            $itemModel = $getStudentItemsRow['model'];
-                                            $itemSN = $getStudentItemsRow['serialnumber'];
-                                            $itemColor = $getStudentItemsRow['color'];
-                                            $isInStatus = $getStudentItemsRow['is_in'];
-                                        ?>
-
-                                            <tr>
-                                                <td><?php echo $itemTypeDesc; ?></td>
-                                                <td><?php echo $itemBrand; ?></td>
-                                                <td><?php echo $itemModel; ?></td>
-                                                <td><?php echo $itemColor; ?></td>
-                                                <td><?php echo $itemSN; ?></td>
-                                                <td><input name="userItem[]" value="12" type="checkbox" checked></td>
-                                            </tr>
-                                        <?php
-                                        }
-                                        ?>
-                                    </tbody>
-                                </table>
-                            </div>
-                            <div id="register-link" class="text-right">
-                                <input type="submit" name="Submit" value="Submit">
-                            </div>
+                                    <?php
+                                    }
+                                    ?>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div id="register-link" class="text-right">
+                            <input type="submit" name="Submit" value="Submit">
+                        </div>
                     </form>
             </div>
         </div>
