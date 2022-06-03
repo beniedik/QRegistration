3b8cf175698340d9d159af545061f127f7cf604c
watermillow321 <89201568+watermillow321@users.noreply.github.com>
2022-06-03 19:55:54 +0800

    Updated UI for student login

--- userlogin.php
+++ userlogin.php
@@ -3,6 +3,37 @@
 //this will be a page where user selects which role can login
 require_once("template/header.php");
 ?>
+<h2 class="text-center text-white"></h2>
+<br />
+<br />
+<br />
+<br />
+<br />
+<br />
+<div class="container">
+  <div id="login-row" class="row justify-content-center align-items-center">
+    <div id="login-column" class="col-md-6">
+      <div id="login-box" class="col-md-12">
+        <form id="login-form" class="form" action="userlogin_processor.php" method="post">
+          <h3 class="text-center text-info">Student Login</h3>
+          <div class="form-group">
+            <label for="username" class="text-info">Username:</label><br>
+            <input type="text" name="usernameField" id="usernameField" placeholder="Username" class="form-control">
+          </div>
+          <div class="form-group">
+            <label for="password" class="text-info">Password:</label><br>
+            <input type="password" name="passwordField" id="passwordField" placeholder="Password" class="form-control">
+          </div>
+          <div id="register-link" class="text-right">
+            <!--<a href="#" class="text-info"><button type="button" class="btn btn-primary">Login</button></a>-->
+            <input class="button expand" type="submit" value="Sign In">
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+<!--
     <div>
       <h2>Student Log In</h2>
       <form action="userlogin_processor.php" method="post">
@@ -19,5 +50,6 @@ require_once("template/header.php");
         <input class="button expand" type="submit" value="Sign In">
       </form>
     </div>
+-->
 <?php
 require_once("template/footer.php");
