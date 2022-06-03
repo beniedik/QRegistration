673addac991e3cd60d5eb9ba60140ea8594a78d0
watermillow321 <89201568+watermillow321@users.noreply.github.com>
2022-06-03 20:24:02 +0800

    Updated UI for item registration

--- regauth.php
+++ regauth.php
@@ -1,11 +1,60 @@
 <?php
-include_once 'template/header.php';
+include_once 'template/header2.php';
 include_once 'template/magic.php';
 include_once 'dbconn.php';
 ?>
+<h2 class="text-center text-white">Item for Approval</h2>
+<div class="container">
+    <div id="login-row" class="row justify-content-center align-items-center">
+        <div id="itemreg-column" class="col-md-12">
+            <div id="itemreg-box" class="col-md-12">
+                <h3 class="text-center text-info">Item for Approval</h3>
+                <div class="form-group">
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th scope="col">Student Name</th>
+                                <th scope="col">Student Number</th>
+                                <th scope="col">Item Type</th>
+                                <th scope="col"></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            include_once 'template/tableheader.php';
+                            $getUnauthRegItemQuery = "select u.useritemid, s.studentname, s.studentidnumber, i.itemtypedesc from itemtype i, studentusers s, useritems u where u.userid=s.userid and u.itemtypeid=i.itemtypeid and u.is_reviewed <> true and u.is_approved <> true order by u.useritemid asc";
+                            $getUnauthRegItemStmt = $dbh->query($getUnauthRegItemQuery) or die(print_r($dbh->errorInfo(), true));
+
+                            foreach ($getUnauthRegItemStmt as $getUnauthRegItemRow) {
+                                //
+                                $userItemIdent = $getUnauthRegItemRow['useritemid'];
+                                $userRealName = $getUnauthRegItemRow['studentname'];
+                                $userStudentNumber = $getUnauthRegItemRow['studentidnumber'];
+                                $userItemType = $getUnauthRegItemRow['itemtypedesc'];
+                            ?>
+                                <tr>
+                                    <td><?php echo $userRealName; ?></td>
+                                    <td><?php echo $userStudentNumber; ?></td>
+                                    <td><?php echo $userItemType; ?></td>
+                                    <td><a href="viewdetailforapproval.php?id=<?php echo $userItemIdent; ?>"><button>View</button></a></td>
+                                </tr>
+                            <?php
+                            }
+                            include_once 'template/tablefooter.php';
+                            ?>
+                </div>
+            </div>
+            <div id="itemreg-box" class="col-md-12">
+                <button>Report</button>
+            </div>
+        </div>
+    </div>
+</div>
+<!--
 <h2>Registration List For Review</h2>
-<div>
-    <?php
+<div>-->
+<?php
+/*
     include_once 'template/tableheader.php';
     $getUnauthRegItemQuery = "select u.useritemid, s.studentname, s.studentidnumber, i.itemtypedesc from itemtype i, studentusers s, useritems u where u.userid=s.userid and u.itemtypeid=i.itemtypeid and u.is_reviewed <> true and u.is_approved <> true order by u.useritemid asc";
     $getUnauthRegItemStmt = $dbh->query($getUnauthRegItemQuery) or die(print_r($dbh->errorInfo(), true));
@@ -15,18 +64,20 @@ include_once 'dbconn.php';
         $userItemIdent = $getUnauthRegItemRow['useritemid'];
         $userRealName = $getUnauthRegItemRow['studentname'];
         $userStudentNumber = $getUnauthRegItemRow['studentidnumber'];
-        $userItemType = $getUnauthRegItemRow['itemtypedesc'];
-    ?>
-        <tr>
+        $userItemType = $getUnauthRegItemRow['itemtypedesc'];*/
+?>
+<!--<tr>
             <td><?php echo $userRealName; ?></td>
             <td><?php echo $userStudentNumber; ?></td>
             <td><?php echo $userItemType; ?></td>
             <td><a href="viewdetailforapproval.php?id=<?php echo $userItemIdent; ?>"><button>View</button></a></td>
-        </tr>
-    <?php
+        </tr>-->
+<?php
+/*
     }
-    include_once 'template/tablefooter.php';
-    ?>
+    include_once 'template/tablefooter.php';*/
+?>
+
 </div>
 <br />
 <br />
