c16e129842aa3c3a423d43ab13c0e2099f3d930b
watermillow321 <89201568+watermillow321@users.noreply.github.com>
2022-06-03 20:27:04 +0800

    Minor UI fix

--- regauth.php
+++ regauth.php
@@ -50,35 +50,6 @@ include_once 'dbconn.php';
         </div>
     </div>
 </div>
-<!--
-<h2>Registration List For Review</h2>
-<div>-->
-<?php
-/*
-    include_once 'template/tableheader.php';
-    $getUnauthRegItemQuery = "select u.useritemid, s.studentname, s.studentidnumber, i.itemtypedesc from itemtype i, studentusers s, useritems u where u.userid=s.userid and u.itemtypeid=i.itemtypeid and u.is_reviewed <> true and u.is_approved <> true order by u.useritemid asc";
-    $getUnauthRegItemStmt = $dbh->query($getUnauthRegItemQuery) or die(print_r($dbh->errorInfo(), true));
-
-    foreach ($getUnauthRegItemStmt as $getUnauthRegItemRow) {
-        //
-        $userItemIdent = $getUnauthRegItemRow['useritemid'];
-        $userRealName = $getUnauthRegItemRow['studentname'];
-        $userStudentNumber = $getUnauthRegItemRow['studentidnumber'];
-        $userItemType = $getUnauthRegItemRow['itemtypedesc'];*/
-?>
-<!--<tr>
-            <td><?php echo $userRealName; ?></td>
-            <td><?php echo $userStudentNumber; ?></td>
-            <td><?php echo $userItemType; ?></td>
-            <td><a href="viewdetailforapproval.php?id=<?php echo $userItemIdent; ?>"><button>View</button></a></td>
-        </tr>-->
-<?php
-/*
-    }
-    include_once 'template/tablefooter.php';*/
-?>
-
-</div>
 <br />
 <br />
 <a href="useritemreport.php" target="_blank">Report</a><br />
