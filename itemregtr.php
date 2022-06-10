f09672d0cf7567a73141cc6d367bed9d70a3c911
beniedik carreon <beniedik051502@gmail.com>
2022-06-10 18:14:49 +0800

    Updated look and additional form input for item picture

--- itemregtr.php
+++ itemregtr.php
@@ -3,17 +3,19 @@ include 'template/header2.php';
 include 'template/magic.php';
 include 'dbconn.php';
 ?>
-<h2 class="text-center text-white">Item Registration</h2>
-<div class="container">
-    <div id="login-row" class="row justify-content-center align-items-center">
-        <div id="itemreg-column" class="col-md-6">
-            <div id="itemreg-box" class="col-md-12">
-                <form id="itemreg-form" class="form" action="itemregtr_processor.php" method="post">
-                    <h3 class="text-center text-info">Item Registration</h3>
-                    <div class="form-group">
-                        <label for="itemType" class="text-info">Item Type:</label><br>
-                        <select class="form-control" name="itemtypeid" id="itemTypeField" required="">
-                            <option value="">Select Your Option</option>
+        <h2 class="text-center text-white">&nbsp;</h2>
+        <div class="container">
+            <div id="login-row" class="row justify-content-center align-items-center">
+                <div id="itemreg-column" class="col-md-6">
+                    <div id="itemreg-box" class="col-md-12">
+						<h3 class="text-center text-info">Item Registration</h3>
+						<div class="card">
+							<div class="card-body">
+								<form id="itemreg-form" class="form" action="http://192.168.1.106/QRegistration/itemregtr_processor.php" method="post">
+									<div class="form-group">
+										<label for="brand" class="text-info">Item Type:</label><br>
+										<select class="form-control" name="itemtypeid" id="itemTypeField" required="">
+											<option value="">Select Your Option</option>
                             <?php
                             $getItemTypesQuery = "select itemtypeid, itemtypedesc from itemtype";
                             $getItemTypesStmt = $dbh->query($getItemTypesQuery) or die(print_r($dbh->errorInfo(), true));
@@ -44,6 +46,21 @@ include 'dbconn.php';
                         <label for="color" class="text-info">Serial Number:</label><br>
                         <input name="itemsn" type="text" placeholder="e.g. ABC123" id="serialnumber" class="form-control">
                     </div>
+                    <div class="form-group">
+                        <label for="itempicturefront" class="text-info">Item Picture (front):</label><br>
+                        <input type="file" class="form-control-file" id="itempicturefront">
+                    </div>
+                    <div class="form-group">
+                        <label for="itempictureback" class="text-info">Item Picture (back):</label><br>
+                        <input type="file" class="form-control-file" id="itempictureback">
+                    </div>
+                    <div class="form-group">
+                        <label for="itempicturesn" class="text-info">Item Picture (Serial Number):</label><br>
+                        <input type="file" class="form-control-file" id="itempicturesn">
+                    </div>							
+                    <div id="register-link" class="text-right">
+                        <a href="#" class="text-info"><button type="button" class="btn btn-primary">Submit</button></a>
+                    </div>                    
                     <div id="register-link" class="text-right">
                         <input class="button expand" type="submit" value="Submit">
                     </div>
