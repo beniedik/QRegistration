ef8ac979969fff169ddf7d6728f8b52bffc6510c
beniedik carreon <beniedik051502@gmail.com>
2022-06-10 16:25:40 +0800

    Added fields for item image URL

--- dbschema/db.sql
+++ dbschema/db.sql
@@ -70,5 +70,8 @@ create table useritems (
 	is_in boolean default null,
 	is_indate timestamptz default null,
 	is_outdate timestamptz default null,
-	is_cancelled boolean default false
+	is_cancelled boolean default false,
+	img_front text default null,
+	img_back text default null,
+	img_sn text default null
 );
