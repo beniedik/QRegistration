da0ef638211e19fa7c66a9597e27afccbc40cd98
beniedik carreon <beniedik051502@gmail.com>
2022-06-10 22:35:58 +0800

    Removed display query and die command

--- itemfeedback.php
+++ itemfeedback.php
@@ -25,8 +25,6 @@ include 'dbconn.php';
                                     <tbody>
 <?php
 $getItemReviewQuery= "select u.useritemid, i.itemtypedesc as itemtypedesc, u.brand as brand, u.model as model, u.serialnumber as serialnumber, u.color as color, u.img_front as ifront, u.img_back as iback, u.img_sn as isn, is_approved, refusal_note from studentusers as s, itemtype as i, useritems as u where u.userid=s.userid and u.itemtypeid=i.itemtypeid and u.userid=$loggedInUserId";
-echo $getItemReviewQuery;
-die();
 $getItemReviewStmt = $dbh->query($getItemReviewQuery) or die(print_r($dbh->errorInfo(), true));
 
 foreach ($getItemReviewStmt as $getItemReviewRow)
