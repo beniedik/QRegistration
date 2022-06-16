493248a59acfade597c70aa0a0bf89f28ec1dcba
beniedik carreon <beniedik051502@gmail.com>
2022-06-16 18:35:18 +0800

    Echo upload status, for testing

--- itemregtr_processor.php
+++ itemregtr_processor.php
@@ -28,49 +28,50 @@ if(isset($submit))
         $img_{{$i}} = $target;
     }
     */
-    extract($_POST);
-    if(isset($submit))
+extract($_POST);
+if(isset($submit))
+{
+    $count = count($_FILES['image']['name']);
+    for($i=0;$i<$count;$i++)
     {
-        $count = count($_FILES['image']['name']);
-        for($i=0;$i<$count;$i++)
-        {
-            $fname = $_FILES['image']['name'][$i];
-            $file_tmp = $_FILES['image']['tmp_name'][$i];
-            $file_size =  $_FILES['image']['size'][$i];
-            $file_type=$_FILES['image']['type'][$i];
-            echo $file_size,$file_type;         
-            $target = "img/".date("Y_m_d_H_i_s").$fname;
-            move_uploaded_file($file_tmp,$target);
-            //echo "uploaded succ !"."<br>";
-            $img_{{$i}} = $target;
-        }
+        $fname = $_FILES['image']['name'][$i];
+        $file_tmp = $_FILES['image']['tmp_name'][$i];
+        $file_size =  $_FILES['image']['size'][$i];
+        $file_type=$_FILES['image']['type'][$i];
+        echo $file_size,$file_type;         
+        $target = "img/".date("Y_m_d_H_i_s").$fname;
+        move_uploaded_file($file_tmp,$target);
+        echo "uploaded succ !"."<br>";
+        //$img_{{$i}} = $target;
     }
-
-    $itemtypeid= $_REQUEST['itemtypeid'];
-    $itembrand= $_REQUEST['itembrand'];
-    $itemmodel= $_REQUEST['itemmodel'];
-    $itemcolor= $_REQUEST['itemcolor'];
-    $itemsn= $_REQUEST['itemsn'];
-
-    $sqlQuery="insert into useritems(userid, itemtypeid, brand, model, serialnumber, color, img_0, img_1, img_2) values($loggedInUserId, $itemtypeid, '$itembrand', '$itemmodel', '$itemsn', '$itemcolor', '$img_0', '$img_1', '$img_2')";
-    echo $sqlQuery;
     die();
+}
 
-    if($itemtypeid <> '')
+$itemtypeid= $_REQUEST['itemtypeid'];
+$itembrand= $_REQUEST['itembrand'];
+$itemmodel= $_REQUEST['itemmodel'];
+$itemcolor= $_REQUEST['itemcolor'];
+$itemsn= $_REQUEST['itemsn'];
+
+$sqlQuery="insert into useritems(userid, itemtypeid, brand, model, serialnumber, color, img_0, img_1, img_2) values($loggedInUserId, $itemtypeid, '$itembrand', '$itemmodel', '$itemsn', '$itemcolor', '$img_0', '$img_1', '$img_2')";
+echo $sqlQuery;
+die();
+
+if($itemtypeid <> '')
+{
+    try
     {
-        try
-        {
-            $dbh->beginTransaction();
-            $dbh->query($sqlQuery);
-            $dbh->commit();
-        }
-        catch(PDOException $e)
-        {
-            $dbh->rollback();
-            echo "Failed to complete transaction: " . $e->getMessage() . "\n";
-            exit;
-        }
+        $dbh->beginTransaction();
+        $dbh->query($sqlQuery);
+        $dbh->commit();
     }
+    catch(PDOException $e)
+    {
+        $dbh->rollback();
+        echo "Failed to complete transaction: " . $e->getMessage() . "\n";
+        exit;
+    }
+}
 //}
 
 $host  = $_SERVER['HTTP_HOST'];
