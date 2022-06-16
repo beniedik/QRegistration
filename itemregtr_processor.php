3dc1b01a35653f0b5dfb19b8034a70ba7cfd5b7f
beniedik carreon <beniedik051502@gmail.com>
2022-06-16 18:27:18 +0800

    Minor fix, for further test

--- itemregtr_processor.php
+++ itemregtr_processor.php
@@ -14,18 +14,18 @@ if(isset($submit))
         $file_tmp = $_FILES['image']['tmp_name'][$i];
         $file_size =  $_FILES['image']['size'][$i];
         $file_type=$_FILES['image']['type'][$i];
+        /*
         if($fileSize/1024 > "2048") {
-            //Its good idea to restrict large files to be uploaded.
+            //restrict large files to be uploaded.
             echo "Filesize is not correct it should equal to 2 MB or less than 2 MB.";
             exit();
-        } //FileSize Checking
-        //echo $file_size,$file_type;
+        }*/
         $target = "stash/".date("Y_m_d_H_i_s").$_FILES["image"]["name"];
-        if($file_type== "image/png" || $file_type== "image/gif" && $file_type== "image/jpg" && $file_type== "image/jpeg" && $count <= 3)
-        {
+        //if($file_type== "image/png" || $file_type== "image/gif" && $file_type== "image/jpg" && $file_type== "image/jpeg" && $count <= 3)
+        //{
             move_uploaded_file($file_tmp, $target);
-        }       
-        img_{{$i}} = $target;
+        //}       
+        $img_{{$i}} = $target;
     }
 
     $itemtypeid= $_REQUEST['itemtypeid'];
@@ -34,7 +34,7 @@ if(isset($submit))
     $itemcolor= $_REQUEST['itemcolor'];
     $itemsn= $_REQUEST['itemsn'];
 
-    $sqlQuery="insert into useritems(userid, itemtypeid, brand, model, serialnumber, color, img_0, img_1, img_2) values($loggedInUserId, $itemtypeid, '$itembrand', '$itemmodel', '$itemsn', '$itemcolor', '$img_0', '$img_1', '$img_3')";
+    $sqlQuery="insert into useritems(userid, itemtypeid, brand, model, serialnumber, color, img_0, img_1, img_2) values($loggedInUserId, $itemtypeid, '$itembrand', '$itemmodel', '$itemsn', '$itemcolor', '$img_0', '$img_1', '$img_2')";
     echo $sqlQuery;
     die();
 
