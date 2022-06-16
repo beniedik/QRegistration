1ebdf95bf0abed01f3fee26bdb3832b82eb1761f
beniedik carreon <beniedik051502@gmail.com>
2022-06-16 15:37:26 +0800

    Logic to add item type into DB

---
+++ additemtype_processor.php
@@ -0,0 +1,26 @@
+<?php
+include 'dbconn.php';
+
+$itemtype= $_REQUEST['itemtype'];
+
+$sqlQuery="insert into itemtype(itemtypedesc) values($itemtype)";
+
+try
+{
+    $dbh->beginTransaction();
+    $dbh->query($sqlQuery);
+    $dbh->commit();
+}
+catch(PDOException $e)
+{
+    $dbh->rollback();
+    echo "Failed to complete transaction: " . $e->getMessage() . "\n";
+    exit;
+}
+
+$host  = $_SERVER['HTTP_HOST'];
+$uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+$extra = 'additemtype.php';
+
+//redirect to secured home page (home.php)
+header("Location:http://$host$uri/$extra");
