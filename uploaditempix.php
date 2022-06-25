8beb2bf5c7be0e1d9f869d0b00b7adb7dd0402db
Beniedik Carreon <beniedik051502@gmail.com>
2022-06-25 23:56:36 +0800

    For further testing

--- uploaditempix.php
+++ uploaditempix.php
@@ -2,12 +2,19 @@
 include 'template/header2.php';
 include 'template/magic.php';
 include 'dbconn.php';
-
+?>
+        <h2 class="text-center text-white">&nbsp;</h2>
+        <div class="container">
+            <div id="login-row" class="row justify-content-center align-items-center">
+                <div id="itemreg-column" class="col-md-6">
+                    <div id="itemreg-box" class="col-md-12">
+                        <form id="itemreg-form" class="form" action="allowuseritem.php" method="post">
+                            <h3 class="text-center text-info">Registered Item Detail Picture Upload</h3>
+<?php
 //Upload item pix
 $userItemId= $_GET['id'];
 $getUserItemDetailQuery = "select s.studentname, s.studentidnumber, i.itemtypedesc as itemtypedesc, u.brand as brand, u.model as model, u.serialnumber as serialnumber, u.color as color from studentusers as s, itemtype as i, useritems as u where u.userid=s.userid and u.itemtypeid=i.itemtypeid and u.useritemid=$userItemId";
-echo "$getUserItemDetailQuery";
-die();
+
 $getUserItemDetailStmt = $dbh->query($getUserItemDetailQuery) or die(print_r($dbh->errorInfo(), true));
 
 foreach ($getUserItemDetailStmt as $getUserItemDetailRow) {
@@ -20,13 +27,7 @@ foreach ($getUserItemDetailStmt as $getUserItemDetailRow) {
     $itemSN = $getUserItemDetailRow['serialnumber'];
     $itemColor = $getUserItemDetailRow['color'];
 ?>
-        <h2 class="text-center text-white">&nbsp;</h2>
-        <div class="container">
-            <div id="login-row" class="row justify-content-center align-items-center">
-                <div id="itemreg-column" class="col-md-6">
-                    <div id="itemreg-box" class="col-md-12">
-                        <form id="itemreg-form" class="form" action="allowuseritem.php" method="post">
-                            <h3 class="text-center text-info">Registered Item Detail Picture Upload</h3>
+
                             <div class="card">
                                 <div class="card-body">
                                     <h5 class="card-title"><?php echo $userRealName; ?></h5>
@@ -39,6 +40,9 @@ foreach ($getUserItemDetailStmt as $getUserItemDetailRow) {
                                         <li class="list-group-item">Serial Number: <?php echo $itemSN; ?></li>
                                     </ul>
                                 </div>
+<?php
+}
+?>                                
                                 <div class="form-group">
                                     <label for="itempicturefront" class="text-info">Item Picture<br/>(Maximum 3 pictures for front, back and serial number):</label><br>
                                     <input type="file" class="form-control-file" id="image" multiple="multiple" name="image[]">
