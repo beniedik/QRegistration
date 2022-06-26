f8d756cdcac7f6a137e9c4a9b34773212ce8dabe
Beniedik Carreon <beniedik051502@gmail.com>
2022-06-27 02:47:49 +0800

    Reuse code from working experiment

--- uploaditempix.php
+++ uploaditempix.php
@@ -8,8 +8,7 @@ include 'dbconn.php';
             <div id="login-row" class="row justify-content-center align-items-center">
                 <div id="itemreg-column" class="col-md-6">
                     <div id="itemreg-box" class="col-md-12">
-                        <form id="itemreg-form" class="form" action="uploaditempix_processor.php" method="post">
-                            <h3 class="text-center text-info">Registered Item Detail Picture Upload</h3>
+						<h3 class="text-center text-info">Registered Item Detail Picture Upload</h3>
 <?php
 //Upload item pix
 $userItemId= $_GET['id'];
@@ -18,56 +17,44 @@ $getUserItemDetailQuery = "select s.studentname, s.studentidnumber, i.itemtypede
 $getUserItemDetailStmt = $dbh->query($getUserItemDetailQuery) or die(print_r($dbh->errorInfo(), true));
 
 foreach ($getUserItemDetailStmt as $getUserItemDetailRow) {
-    $userIdent = $userItemId;
-    $userRealName = $getUserItemDetailRow['studentname'];
-    $userStudentNumber = $getUserItemDetailRow['studentidnumber'];
-    $itemDesc = $getUserItemDetailRow['itemtypedesc'];
-    $itemBrand = $getUserItemDetailRow['brand'];
-    $itemModel = $getUserItemDetailRow['model'];
-    $itemSN = $getUserItemDetailRow['serialnumber'];
-    $itemColor = $getUserItemDetailRow['color'];
+	$userIdent = $userItemId;
+	$userRealName = $getUserItemDetailRow['studentname'];
+	$userStudentNumber = $getUserItemDetailRow['studentidnumber'];
+	$itemDesc = $getUserItemDetailRow['itemtypedesc'];
+	$itemBrand = $getUserItemDetailRow['brand'];
+	$itemModel = $getUserItemDetailRow['model'];
+	$itemSN = $getUserItemDetailRow['serialnumber'];
+	$itemColor = $getUserItemDetailRow['color'];
 ?>
 
-                            <div class="card">
-                                <div class="card-body">
-                                    <h5 class="card-title"><?php echo $userRealName; ?></h5>
-                                    <h6 class="card-subtitle mb-2 text-muted"><?php echo $userStudentNumber; ?></h6>
-                                    <ul class="list-group list-group-flush">
-                                        <li class="list-group-item">Item Type: <?php echo $itemDesc; ?></li>
-                                        <li class="list-group-item">Brand: <?php echo $itemBrand; ?></li>
-                                        <li class="list-group-item">Model: <?php echo $itemModel; ?></li>
-                                        <li class="list-group-item">Color: <?php echo $itemColor; ?></li>
-                                        <li class="list-group-item">Serial Number: <?php echo $itemSN; ?></li>
-                                    </ul>
+						<div class="card">
+							<div class="card-body">
+								<h5 class="card-title"><?php echo $userRealName; ?></h5>
+								<h6 class="card-subtitle mb-2 text-muted"><?php echo $userStudentNumber; ?></h6>
+								<ul class="list-group list-group-flush">
+									<li class="list-group-item">Item Type: <?php echo $itemDesc; ?></li>
+									<li class="list-group-item">Brand: <?php echo $itemBrand; ?></li>
+									<li class="list-group-item">Model: <?php echo $itemModel; ?></li>
+									<li class="list-group-item">Color: <?php echo $itemColor; ?></li>
+									<li class="list-group-item">Serial Number: <?php echo $itemSN; ?></li>
+								</ul>
 
 <?php
 }
-?>                                
-
-                                    <div class="form-group">
-                                        <input type="hidden" name="useritemid" id="useritemid" value="<?php echo $userItemId; ?>">
-                                        <label for="itempicturefront" class="text-info">Item Picture<br/>(Maximum 3 pictures for front, back and serial number, should be 2MB maximum filesize only):</label><br>
-                                        <input type="file" name="image[]" id="image" class="form-control-file" multiple="multiple">
-                                    </div>						
-                                    <div id="register-link" class="text-right">
-                                        <input class="button expand" type="submit" name="submit" value="Submit">
-                                    </div>
-                                </div>
-                            </div>
-                        </form>
+?> 						
+								<div class="form-group">						
+									<form action="uploaditempix_processor.php" method="post" enctype="multipart/form-data">
+										<label for="image">Picture file (should be less than 500KB):</label>
+										<input type="hidden" name="useritemid" id="useritemid" value="<?php echo $userItemId; ?>">
+										<input type="file" name="image" id="image"><br/>
+										<input type="submit" name="submit" value="Upload">
+									</form>
+								</div>
+							</div>
+						</div>
                     </div>
                 </div>
             </div>
         </div>
 <?php
-include_once 'template/footer.php';                                
-
-
-//get item details
-
-
-//display item details
-
-//UI for pic upload
-
-//redirect to item feedback
+include_once 'template/footer.php';									
--- uploaditempix_processor.php
+++ uploaditempix_processor.php
@@ -1,76 +1,55 @@
 <?php
-if(isset($_POST['submit'])) {           
-	foreach($_FILES['image'] as $file){ 
-		if ((($file["type"] == "image/jpeg") || ($file["type"] == "image/pjpeg") || (file["type"] == "image/png")) || (file["type"] == "image/gif")))
-		{
-			if ($file["error"] > 0)
-			{
-				echo "Return Code: " . $file["error"] . "<br />";
-			}
-			else
-			{
-				if (file_exists("upload/" . $file["name"]))
-				{
-					echo $file["name"] . " already exists. ";
-				}
-				else
-				{
-					move_uploaded_file($_FILES["file"]["tmp_name"], "stash/" . $file["name"]);
-					echo "Stored in: " . "stash/" . $file["name"];
-				}
-			}
-		}
-		else
-		{
-		echo "Invalid file";
-		}
-	}
+include 'dbconn.php';
+
+$userItemId = $_POST['useritemid'];
+
+$target_dir = "stash/";
+$target_file = $target_dir . basename($_FILES["image"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+  $check = getimagesize($_FILES["image"]["tmp_name"]);
+  if($check !== false) {
+    echo "File is an image - " . $check["mime"] . ".";
+    $uploadOk = 1;
+  } else {
+    echo "File is not an image.";
+    $uploadOk = 0;
+  }
 }
-//if ($_SERVER['REQUEST_METHOD'] === 'POST')
-//if (isset($_POST['submit']))
-//{
-	//$file_array= array();
 
-	//if (isset($_FILES['image'])) {
-	//	$path = 'stash/'; //path you wish to store you uploaded files
-	//	$total_files = count($_FILES['image']['name']);
-	//	echo "Number of files: $total_files";
-	//	die();
-
-		
-/*
-		for($key = 0; $key < $total_files; $key++)
-		{
-			$file_name = $_FILES['image']['name'][$key];
-			$enc_filename = md5($_FILES['image']['name'][$key]);
-			$file_tmp = $_FILES['image']['tmp_name'][$key];
-			$file_type = $_FILES['image']['type'][$key];
-			$file_ext = strtolower(end(explode('.', $file_name)));
-			$file = $path . $enc_filename . "." . $file_ext;
-			
-			if(move_uploaded_file($file_tmp, $file))
-			{
-				$file_array[] = $file;
-			} else {
-				echo 'There was a problem saving the uploaded file';
-			}
-		}
-	}
-//}
-
-if(sizeof($file_array) > 0)
-{
-	var_dump($file_array);
+// Check if file already exists
+if (file_exists($target_file)) {
+  echo "Sorry, file already exists.";
+  $uploadOk = 0;
 }
-else
-{
-	echo "No file uploaded";
-	die();
-}
-*/
-$host  = $_SERVER['HTTP_HOST'];
-$uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
-$extra = 'itemfeedback.php';
 
-//redirect to secured home page (home.php)
-header("Location:http://$host$uri/$extra");
+// Check file size
+if ($_FILES["image"]["size"] > 500000) {
+  echo "Sorry, your file is too large.";
+  $uploadOk = 0;
+}
+
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) {
+  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+  $uploadOk = 0;
+}
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+  echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+  if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
+    echo "The file ". htmlspecialchars( basename( $_FILES["image"]["name"])). " has been uploaded.";
+	//enter $target_file to db
+	
+	//update is_forapproval to true
+	//jump to itemfeedback
+  } else {
+    echo "Sorry, there was an error uploading your file.";
+  }
+}
