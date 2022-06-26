0fcefa7ababeb349566c664cb810a9bc89155839
Beniedik Carreon <beniedik051502@gmail.com>
2022-06-27 03:00:16 +0800

    Added logic for db queries and jump to another page

--- uploaditempix_processor.php
+++ uploaditempix_processor.php
@@ -10,46 +10,59 @@ $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
-  $check = getimagesize($_FILES["image"]["tmp_name"]);
-  if($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
-    $uploadOk = 1;
-  } else {
-    echo "File is not an image.";
-    $uploadOk = 0;
-  }
+	$check = getimagesize($_FILES["image"]["tmp_name"]);
+	if($check !== false) {
+		echo "File is an image - " . $check["mime"] . ".";
+		$uploadOk = 1;
+	} else {
+		echo "File is not an image.";
+		$uploadOk = 0;
+	}
 }
 
 // Check if file already exists
 if (file_exists($target_file)) {
-  echo "Sorry, file already exists.";
-  $uploadOk = 0;
+	echo "Sorry, file already exists.";
+	$uploadOk = 0;
 }
 
 // Check file size
 if ($_FILES["image"]["size"] > 500000) {
-  echo "Sorry, your file is too large.";
-  $uploadOk = 0;
+	echo "Sorry, your file is too large.";
+	$uploadOk = 0;
 }
 
 // Allow certain file formats
 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) {
-  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-  $uploadOk = 0;
+	echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+	$uploadOk = 0;
 }
 
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
-  echo "Sorry, your file was not uploaded.";
+	echo "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
-} else {
-  if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
-    echo "The file ". htmlspecialchars( basename( $_FILES["image"]["name"])). " has been uploaded.";
-	//enter $target_file to db
-	
-	//update is_forapproval to true
-	//jump to itemfeedback
-  } else {
-    echo "Sorry, there was an error uploading your file.";
-  }
+}
+else
+{
+	if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
+		//echo "The file ". htmlspecialchars( basename( $_FILES["image"]["name"])). " has been uploaded.";
+		//enter $target_file to db
+		$insertToItemPixQuery= "insert into useritempix(useritemid, pixurl) values($userItemId, $target_file)";
+		$dbh->query($insertToItemPixQuery) or die(print_r($dbh->errorInfo(), true));
+
+		//update is_forapproval to true
+		$updateUserItemQuery= "update useritems set is_forapproval=true where useritemid=$userItemId";
+		$dbh->query($updateUserItemQuery) or die(print_r($dbh->errorInfo(), true));
+
+		//jump to itemfeedback
+		$host  = $_SERVER['HTTP_HOST'];
+		$uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+		$extra = 'itemfeedback.php';
+
+		//redirect to secured home page (home.php)
+		header("Location:http://$host$uri/$extra");	
+	} else {
+		echo "Sorry, there was an error uploading your file.";
+	}
 }
