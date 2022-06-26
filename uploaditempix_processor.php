80fdec18690a4586cdbc583e7829b26b6938881a
Beniedik Carreon <beniedik051502@gmail.com>
2022-06-26 22:59:34 +0800

    For testing

--- uploaditempix_processor.php
+++ uploaditempix_processor.php
@@ -1,22 +1,26 @@
 <?php
-$file_array= array();
-if (isset($_FILES['image'])) {
-	$path = 'stash/'; //path you wish to store you uploaded files
-	$total_files = count($_FILES['image']['name']);
-	for($key = 0; $key < $total_files; $key++)
-	{
-		$file_name = $_FILES['image']['name'][$key];
-		$enc_filename = md5($_FILES['image']['name'][$key]);
-		$file_tmp = $_FILES['image']['tmp_name'][$key];
-		$file_type = $_FILES['image']['type'][$key];
-		$file_ext = strtolower(end(explode('.', $file_name)));
-		$file = $path . $enc_filename . "." . $file_ext;
-		
-		if(move_uploaded_file($file_tmp, $file))
+if ($_SERVER['REQUEST_METHOD'] === 'POST')
+{
+	$file_array= array();
+
+	if (isset($_FILES['image'])) {
+		$path = 'stash/'; //path you wish to store you uploaded files
+		$total_files = count($_FILES['image']['name']);
+		for($key = 0; $key < $total_files; $key++)
 		{
-			$file_array[] = $file;
-		} else {
-			echo 'There was a problem saving the uploaded file';
+			$file_name = $_FILES['image']['name'][$key];
+			$enc_filename = md5($_FILES['image']['name'][$key]);
+			$file_tmp = $_FILES['image']['tmp_name'][$key];
+			$file_type = $_FILES['image']['type'][$key];
+			$file_ext = strtolower(end(explode('.', $file_name)));
+			$file = $path . $enc_filename . "." . $file_ext;
+			
+			if(move_uploaded_file($file_tmp, $file))
+			{
+				$file_array[] = $file;
+			} else {
+				echo 'There was a problem saving the uploaded file';
+			}
 		}
 	}
 }
