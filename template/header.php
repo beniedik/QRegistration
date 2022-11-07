d0ba9690af488b0488e9ae860ec376b12e0927a3
beniedik <106514458+beniedik@users.noreply.github.com>
2022-11-07 21:16:02 +0800

    Update header.php

--- template/header.php
+++ template/header.php
@@ -18,12 +18,13 @@
     <link rel="stylesheet" href="css/bootstrap-grid.min.css">
     <style>
         body {
-          
+          <div id="content" align="center">
             background: url('img/apc.jpg') no-repeat center center fixed;
             -webkit-background-size: cover;
             -moz-background-size: cover;
             background-size: cover;
             -o-background-size: cover;
+            </div>
         }
     </style>
 
