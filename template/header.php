d80f0d8adeb1eb826df3337d6518f5cba7315b38
beniedik <106514458+beniedik@users.noreply.github.com>
2022-11-07 22:41:40 +0800

    Update header.php

--- template/header.php
+++ template/header.php
@@ -19,7 +19,7 @@
     <style>
    
          body {
-            background: url('img/apcrams.png') no-repeat center center fixed;
+            background: url('img/apc.jpg') no-repeat center center fixed;
             -webkit-background-size: cover;
             -moz-background-size: cover;
             background-size: cover;
