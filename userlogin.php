3088ee3659b5999f4fb228caca0d45856a1b787d
beniedik <106514458+beniedik@users.noreply.github.com>
2022-11-07 20:40:00 +0800

    Update userlogin.php

--- userlogin.php
+++ userlogin.php
@@ -16,7 +16,7 @@ require_once("template/header.php");
     <div id="login-column" class="col-md-6">
       <div id="login-box" class="col-md-12">
         <form id="login-form" class="form" action="userlogin_processor.php" method="post">
-          <h3 class="text-center text-info">Student Login Hello</h3>
+          <h3 class="text-center text-info">Student Login</h3>
           <div class="form-group">
             <label for="username" class="text-info">Username:</label><br>
             <input type="text" name="usernameField" id="usernameField" placeholder="Username" class="form-control">
