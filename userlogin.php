c40602973860290dd9740af301dc8389cbd94c42
beniedik <106514458+beniedik@users.noreply.github.com>
2022-11-07 20:32:07 +0800

    Update userlogin.php

--- userlogin.php
+++ userlogin.php
@@ -10,6 +10,36 @@ require_once("template/header.php");
 <br />
 <br />
 <br />
+<div class="limiter">
+	<div class="container-login100" style="background-image: url('QRegistration/wonderams.ico');">
+			<div class="wrap-login100 p-t-30 p-b-50">
+				<span class="login100-form-title p-b-41">
+					Account Login
+				</span>
+				<form class="login100-form validate-form p-b-33 p-t-5">
+
+					<div class="wrap-input100 validate-input" data-validate = "Enter username">
+						<input class="input100" type="text" name="username" placeholder="User name">
+						<span class="focus-input100" data-placeholder="&#xe82a;"></span>
+					</div>
+
+					<div class="wrap-input100 validate-input" data-validate="Enter password">
+						<input class="input100" type="password" name="pass" placeholder="Password">
+						<span class="focus-input100" data-placeholder="&#xe80f;"></span>
+					</div>
+
+					<div class="container-login100-form-btn m-t-32">
+						<button class="login100-form-btn">
+							Login
+						</button>
+					</div>
+
+				</form>
+			</div>
+		</div>
+	</div>
+	
+	<div id="dropDownSelect1"></div>
 <div class="container">
   <div id="login-row" class="row justify-content-center align-items-center">
     <div id="login-column" class="col-md-6">
