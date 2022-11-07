ce354267c0b70f7fce73d539bf7ec0350bab4cbb
beniedik <106514458+beniedik@users.noreply.github.com>
2022-11-07 21:09:14 +0800

    Update header.php

--- template/header.php
+++ template/header.php
@@ -12,6 +12,7 @@
     <title></title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/x-icon" href="wonderams.ico" />
 
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/bootstrap-grid.min.css">
